7a51ed11ff129697de107822bd0563395ed05e24
Todd Fredrich <tfredrich@gmail.com>
2011-04-20 09:27:58 -0600

    Began introducing FluentRouteBuilder.

--- src/java/com/strategicgains/restexpress/route/RouteDeclaration.java
+++ src/java/com/strategicgains/restexpress/route/RouteDeclaration.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.strategicgains.restexpress.domain.metadata.RouteMetadata;
+import com.strategicgains.restexpress.route.fluent.FluentRouteBuilder;
 import com.strategicgains.restexpress.route.parameterized.ParameterizedRouteBuilder;
 import com.strategicgains.restexpress.route.regex.RegexRouteBuilder;
 
@@ -65,6 +66,13 @@ public abstract class RouteDeclaration
 	public RouteBuilder regex(String regex, Object controller)
 	{
 		RouteBuilder builder = new RegexRouteBuilder(regex, controller);
+		routeBuilders.add(builder);
+		return builder;
+	}
+	
+	public RouteBuilder route()
+	{
+		RouteBuilder builder = new FluentRouteBuilder();
 		routeBuilders.add(builder);
 		return builder;
 	}
