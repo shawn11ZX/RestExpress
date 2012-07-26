79b4a2d7677bce852fa6c5b86f7b2d4413280b29
Todd Fredrich <tfredrich@gmail.com>
2012-07-26 17:28:39 -0600

    Combined CorsHeaderPostprocessor into CorsHeaderPlugin as an inner class.

--- src/java/com/strategicgains/restexpress/plugin/CorsHeaderPlugin.java
+++ src/java/com/strategicgains/restexpress/plugin/CorsHeaderPlugin.java
@@ -3,10 +3,26 @@
  */
 package com.strategicgains.restexpress.plugin;
 
+import com.strategicgains.restexpress.Request;
+import com.strategicgains.restexpress.Response;
 import com.strategicgains.restexpress.RestExpress;
-import com.strategicgains.restexpress.postprocessor.CorsHeaderPostprocessor;
+import com.strategicgains.restexpress.pipeline.Postprocessor;
+import com.strategicgains.restexpress.util.StringUtils;
 
 /**
+ * This RestExpress plugin supports CORS behavior by setting the 'Access-Control-Allow-Origin'
+ * header on the response for GET requests.
+ * <p/>
+ * Usage requires passing in the appropriate domain names that are allowed to perform cross-domain
+ * behaviors.  To support cross-domain behavior globally (not recommended) send in '*'.
+ * <p/>
+ * Usage:
+ * RestExpress server = new RestExpress();
+ * ...
+ * new CorsHeaderPlugin("*").register(server);
+ * or...
+ * server.registerPlugin(new CorsHeaderPlugin("*"));
+ * 
  * @author toddf
  * @since Jun 8, 2012
  */
@@ -35,5 +51,28 @@ extends AbstractPlugin
 	public void bind(RestExpress server)
 	{
 		// do nothing during bind().
+	}
+	
+	private class CorsHeaderPostprocessor
+	implements Postprocessor
+	{
+	    private static final String CORS_ORIGIN_HEADER = "Access-Control-Allow-Origin";
+
+	    private String corsHeader = null;
+
+		public CorsHeaderPostprocessor(String... originDomains)
+		{
+			super();
+			corsHeader = StringUtils.join(" ", (Object[]) originDomains);
+		}
+
+		@Override
+		public void process(Request request, Response response)
+		{
+			if (request.isMethodGet() && corsHeader != null)
+			{
+				response.addHeader(CORS_ORIGIN_HEADER, corsHeader);
+			}
+		}
 	}
 }
