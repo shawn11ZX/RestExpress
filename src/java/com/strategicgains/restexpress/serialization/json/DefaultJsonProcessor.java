1f86099cd1052bee6f262fc2bda6ce11ade1aa25
Todd Fredrich <tfredrich@gmail.com>
2013-01-24 11:39:15 -0700

    Added REQUIRE_SETTERS_FOR_GETTERS as a default for ObjectMapper (JSON handling).

--- src/java/com/strategicgains/restexpress/serialization/json/DefaultJsonProcessor.java
+++ src/java/com/strategicgains/restexpress/serialization/json/DefaultJsonProcessor.java
@@ -28,6 +28,7 @@ import org.jboss.netty.buffer.ChannelBufferInputStream;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -99,6 +100,7 @@ implements SerializationProcessor
 	protected void initializeMapper(ObjectMapper mapper)
     {
 		mapper
+			.enable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS)
 			.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
 			.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
 			.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
