b1372ebecd35135007c852fed25c8d9987d3d705
Todd Fredrich <tfredrich@gmail.com>
2013-02-28 16:08:24 -0700

    Reformatted whitespace.

--- src/java/com/strategicgains/restexpress/preprocessor/HttpBasicAuthenticationPreprocessor.java
+++ src/java/com/strategicgains/restexpress/preprocessor/HttpBasicAuthenticationPreprocessor.java
@@ -12,7 +12,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package com.strategicgains.restexpress.preprocessor;
 
 import javax.xml.bind.DatatypeConverter;
@@ -24,18 +24,21 @@ import com.strategicgains.restexpress.exception.UnauthorizedException;
 import com.strategicgains.restexpress.pipeline.Preprocessor;
 
 /**
- * This preprocessor implements HTTP Basic authentication.  To use it, simply add it to your server as follows:
- * <code>
+ * This preprocessor implements HTTP Basic authentication. To use it, simply add
+ * it to your server as follows: <code>
  * server.addPreprocessor(new HttpBasicAuthenticationPreprocessor("my realm"));
  * </code>
  * <p/>
- * Once this preprocessor completes successfully, it places the username and password in the request as headers,
- * X-AuthenticatedUser and X-AuthenticatedPassword, respectively.
+ * Once this preprocessor completes successfully, it places the username and
+ * password in the request as headers, X-AuthenticatedUser and
+ * X-AuthenticatedPassword, respectively.
  * <p/>
- * If the preprocessor fails, it throws UnauthorizedException, which results in an HTTP status of 401 to the caller.
+ * If the preprocessor fails, it throws UnauthorizedException, which results in
+ * an HTTP status of 401 to the caller.
  * <p/>
- * Use of this preprocessor assumes you'll implement an authorization preprocessor that validations the username and 
- * password extracted from the Authorization header in the request.
+ * Use of this preprocessor assumes you'll implement an authorization
+ * preprocessor that validates the username and password extracted from the
+ * Authorization header in the request.
  * 
  * @author toddf
  * @since Feb 28, 2013
@@ -49,9 +52,12 @@ implements Preprocessor
 	private String realm;
 
 	/**
-	 * Utilize HTTP Basic Authentication with the given realm returned on an unauthenticated request.
+	 * Utilize HTTP Basic Authentication with the given realm returned on an
+	 * unauthenticated request.
 	 * 
-	 * @param realm any value to identify the secure area and may used by HTTP clients to manage passwords.
+	 * @param realm
+	 *            any value to identify the secure area and may used by HTTP
+	 *            clients to manage passwords.
 	 */
 	public HttpBasicAuthenticationPreprocessor(String realm)
 	{
@@ -60,10 +66,10 @@ implements Preprocessor
 	}
 
 	@Override
-    public void process(Request request)
-    {
+	public void process(Request request)
+	{
 		String authorization = request.getRawHeader(HttpHeaders.Names.AUTHORIZATION);
-		
+
 		if (authorization == null || !authorization.startsWith("Basic "))
 		{
 			UnauthorizedException e = new UnauthorizedException("Authenticationrequired");
@@ -75,7 +81,7 @@ implements Preprocessor
 		byte[] bytes = DatatypeConverter.parseBase64Binary(pieces[1]);
 		String credentials = new String(bytes);
 		String[] parts = credentials.split(":");
-		
+
 		if (parts.length < 2)
 		{
 			throw new UnauthorizedException("Authenticationrequired");
@@ -83,5 +89,5 @@ implements Preprocessor
 
 		request.addHeader(X_AUTHENTICATED_USER, parts[0]);
 		request.addHeader(X_AUTHENTICATED_PASSWORD, parts[1]);
-    }
+	}
 }
