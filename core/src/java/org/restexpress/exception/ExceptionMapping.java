7286b55060cf6359bd64aaf81199fb7b1c7d27cb
Todd Fredrich <tfredrich@gmail.com>
2014-02-17 15:26:55 -0700

    Refactored ExceptionMapping an interface, extracting previous implementation into DefaultExceptionMapper.

--- core/src/java/org/restexpress/exception/ExceptionMapping.java
+++ core/src/java/org/restexpress/exception/ExceptionMapping.java
@@ -15,44 +15,17 @@
 */
 package org.restexpress.exception;
 
-import java.lang.reflect.Constructor;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * Implements a user-defined mapping from Throwables to ServiceExceptions so clients can utilize Libraries that
- * throw checked exceptions and map those into a ServiceException that will return an HTTP status code.
+ * Defines an implementation for a user-defined mapping from Throwables to ServiceExceptions so clients
+ * can utilize Libraries that throw checked exceptions and map those into a ServiceException that will
+ * return an HTTP status code.
  * 
  * @author toddf
  * @since Oct 13, 2010
  */
-public class ExceptionMapping
+public interface ExceptionMapping
 {
-	private Map<Class<? extends Throwable>, Class<? extends ServiceException>> exceptions =
-		new HashMap<Class<? extends Throwable>, Class<? extends ServiceException>>();
-	
-	public <T extends Throwable, U extends ServiceException> void map(Class<T> inExceptionClass, Class<U> outExceptionClass)
-	{
-		exceptions.put(inExceptionClass, outExceptionClass);
-	}
-
-	public ServiceException getExceptionFor(Throwable throwable)
-	{
-		Class<?> mapped = exceptions.get(throwable.getClass());
-		
-		if (mapped != null)
-		{
-			try
-            {
-	            Constructor<?> constructor = mapped.getConstructor(String.class, Throwable.class);
-	            return (ServiceException) constructor.newInstance(throwable.getMessage(), throwable);
-            }
-            catch (Exception e)
-            {
-	            e.printStackTrace();
-            }
-		}
-		
-		return null;
-	}
+	public <T extends Throwable, U extends ServiceException> void map(Class<T> inExceptionClass, Class<U> outExceptionClass);
+	public ServiceException getExceptionFor(Throwable throwable);
 }
