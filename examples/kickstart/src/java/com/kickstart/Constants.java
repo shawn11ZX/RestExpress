5da9ce3de039db12197d25fd17407db09695aaae
Todd Fredrich <tfredrich@gmail.com>
2014-11-12 13:18:50 -0700

    Removed the examples/ directory, moving them into their own repository.

--- examples/kickstart/src/java/com/kickstart/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.kickstart;
-
-public class Constants
-{
-	public static final String KICKSTART_ORDER_URI = "KickstartOrderUri";
-	public static final String ORDER_ID_PARAMETER = "orderId";
-}
